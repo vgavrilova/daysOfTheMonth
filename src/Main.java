dd3ffaa579a936904d23a4e7a11c3909de301ee7
vgavrilova <gavrivalery@gmail.com>
2020-05-27 16:17:42 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int days = LeapYear.getDaysInMonth(4, 2021);
+
+        System.out.println("There are " + days + " days in this month");
+    }
+}
